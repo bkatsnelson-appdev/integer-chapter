964f28f3276cc041d341c78c1663bb4605454451
beatricek222 <beatricek222@gmail.com>
2022-10-07 22:57:54 +0000

    Passed integer tests

--- integer_birth_year.rb
+++ integer_birth_year.rb
@@ -1,9 +1,12 @@
 # Ask for a person’s age, and then calculate and display what year they were born in.
-# 
+#
 # Input:
 #   80
-# 
+#
 # Key output:
 #   "Wow, you were born in 1940. You're old!"
 
 p "How old are you?"
+age = gets.chomp
+birth_year = 2022 - age.to_i
+p "Wow, you were born in #{birth_year}. You're old!"
